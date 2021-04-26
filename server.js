28ff6114faf36a68abf4daf7ac46480b5a9a5e2c
Innocent Tauzeni <innocent.tauzeni@gmail.com>
2021-04-26 12:13:43 +0200

    freee

--- server.js
+++ server.js
@@ -1,39 +1,23 @@
-// server.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
+app.use(cors({optionsSuccessStatus: 200}));  
 
 
-// your first API endpoint... 
-
-
-app.get("/api/timestamp/", (req, res) => {
-  res.json({ unix: Date.now(), utc: Date() });
+app.get("/api/timestamp/",(req, res) => {
+  res.json({ unix:new  Date.now(), utc: new Date().toUTCString() });
 });
 
 app.get("/api/timestamp/:date_string", (req, res) => {
   let dateString = req.params.date_string;
 
- 
+  //A 4 digit number is a valid ISO-8601 for the beginning of that year
+  //5 digits or more must be a unix time, until we reach a year 10,000 problem
   if (/\d{5,}/.test(dateString)) {
-    const dateInt = parseInt(dateString);
-    
-    res.json({ unix: dateInt, utc: new Date(dateInt).toUTCString() });
+    let dateInt = parseInt(dateString);
+    //Date regards numbers as unix timestamps, strings are processed differently
+    res.json({ unix: dateString, utc: new Date(dateInt).toUTCString() });
   } else {
     let dateObject = new Date(dateString);
 
@@ -44,8 +28,6 @@ app.get("/api/timestamp/:date_string", (req, res) => {
     }
   }
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
